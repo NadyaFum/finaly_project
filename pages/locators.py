e0b8836034948952c3924e2f8f895e1c90642bdb
NadyaFum <nadyafum@gmail.com>
2022-11-01 16:45:53 +0300

    add the LoginPage

---
+++ pages/locators.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
+
+
+class ProductPageLocators():
+    ADD_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong")
+    MESSAGE_CART_PRICE = (By.CSS_SELECTOR, "#messages div:nth-child(3) strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main .price_color")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
